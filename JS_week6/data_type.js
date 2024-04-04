060d19e6ac6627dc95f2e57f7a6b8140326751fe
FrancesscaWY <3449997118@qq.com>
2024-04-04 08:14:07 +0800

    CHANGE_TYPE...

--- JS_week6/data_type.js
+++ JS_week6/data_type.js
@@ -1,13 +1,14 @@
-console.log('Data Type');
-function isArray(myArray){
-    return myArray.constructor.toString().indexOf("Array")>-1;
-}
-var fruits= ["Banana", "Orange", "Apple", "Mango"];
-document.getElementById("constructor..").innerHTML = isArray(fruits);
+// console.log('Data Type');
+// function isArray(myArray){
+//     return myArray.constructor.toString().indexOf("Array")>-1;
+// }
+// var fruits= ["Banana", "Orange", "Apple", "Mango"];
+// document.getElementById("constructor..").innerHTML = isArray(fruits);
 
 
 
 var f=2;
+var t=2;
 //why not show always show the result??
 var Car ={
     name:"Disco",
@@ -19,6 +20,14 @@ var Car ={
         let text=`<video autoplay loop width=100% height=100%> <source src="src/video/car`+f+`.mp4" type="video/mp4"></video>`;
             document.getElementById("car").innerHTML=text;
             f++;
+    },
+    changeType: function(){
+        document.getElementById("car").style.backgroundColor="black";
+        if(t>6){
+            t=1;
+        }
+        let text1=`<img src="src/picture/type`+t+`.svg" width=100% height=100% alt="There is no type.">`;
+        document.getElementById("car").innerHTML=text1;
+        t++;
     }
-
 }
